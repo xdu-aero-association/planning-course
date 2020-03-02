dd1b0bf85fd6c273c04cce1a60f681bde3e240df
zhangcy2 <zhangcy2@xiaopeng.com>
2020-03-02 22:12:21 +0800

    Finished hw5 matlab part 1, all results correct

--- hw_5_zcy/matlab/hw1_1.m
+++ hw_5_zcy/matlab/hw1_1.m
@@ -1,5 +1,5 @@
 clc;clear;close all;
-path = ginput() * 100.0;
+path = ginput() * 100.0; % [x0,y0;x1,y1; ... ;xn,yn]
 
 n_order       = 7;% order of poly
 n_seg         = size(path,1)-1;% segment number
@@ -7,20 +7,20 @@ n_poly_perseg = (n_order+1); % coef number of perseg
 
 ts = zeros(n_seg, 1);
 % calculate time distribution in proportion to distance between 2 points
-% dist     = zeros(n_seg, 1);
-% dist_sum = 0;
-% T        = 25;
-% t_sum    = 0;
-% 
-% for i = 1:n_seg
-%     dist(i) = sqrt((path(i+1, 1)-path(i, 1))^2 + (path(i+1, 2) - path(i, 2))^2);
-%     dist_sum = dist_sum+dist(i);
-% end
-% for i = 1:n_seg-1
-%     ts(i) = dist(i)/dist_sum*T;
-%     t_sum = t_sum+ts(i);
-% end
-% ts(n_seg) = T - t_sum;
+dist     = zeros(n_seg, 1);
+dist_sum = 0;
+T        = 25;
+t_sum    = 0;
+
+for i = 1:n_seg
+    dist(i) = sqrt((path(i+1, 1)-path(i, 1))^2 + (path(i+1, 2) - path(i, 2))^2);
+    dist_sum = dist_sum+dist(i);
+end
+for i = 1:n_seg-1
+    ts(i) = dist(i)/dist_sum*T;
+    t_sum = t_sum+ts(i);
+end
+ts(n_seg) = T - t_sum;
 
 % or you can simply set all time distribution as 1
 for i = 1:n_seg
@@ -40,6 +40,10 @@ for i=0:n_seg-1
     %#####################################################
     % STEP 3: get the coefficients of i-th segment of both x-axis
     % and y-axis
+    Pxi = flipud(poly_coef_x(1+i*(n_order+1):(i+1)*(n_order+1)));
+    Pyi = flipud(poly_coef_y(1+i*(n_order+1):(i+1)*(n_order+1)));
+%     Pxi = (poly_coef_x(1+i*(n_order+1):(i+1)*(n_order+1)));
+%     Pyi = (poly_coef_y(1+i*(n_order+1):(i+1)*(n_order+1)));
     for t = 0:tstep:ts(i+1)
         X_n(k)  = polyval(Pxi, t);
         Y_n(k)  = polyval(Pyi, t);
@@ -57,9 +61,15 @@ function poly_coef = MinimumSnapQPSolver(waypoints, ts, n_seg, n_order)
     %#####################################################
     % STEP 1: compute Q of p'Qp
     Q = getQ(n_seg, n_order, ts);
+    assignin('base','Q',Q);
     %#####################################################
     % STEP 2: compute Aeq and beq 
     [Aeq, beq] = getAbeq(n_seg, n_order, waypoints, ts, start_cond, end_cond);
+    assignin('base','Aeq',Aeq);
+    assignin('base','beq',beq);
     f = zeros(size(Q,1),1);
+    fprintf(1,'Aeq size: %d, %d\n',size(Aeq,1),size(Aeq,2));
+    fprintf(1,'beq size: %d, %d\n',size(beq,1),size(beq,2));
+    fprintf(1,'Q size: %d, %d\n',size(Q,1),size(Q,2));
     poly_coef = quadprog(Q,f,[],[],Aeq, beq);
 end
